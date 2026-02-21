ab65b145ca24a05095c396ea56e0d38c8bce60c7
layesow <abdoulayesow716@gmail.com>
2026-02-21 21:15:03 +0000

    changement de stateless en statefull car nous devrons avoir des modification sur la botombar

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,36 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home : Scaffold(
         appBar: AppBar(
-          title: const Text("Sallon-Conf"),
+          title: const Text(
+            "Sallon-Conf",
+            style: TextStyle(
+              color: Colors.white
+            ),
+          ),
+          backgroundColor: Colors.blue,
         ),
         body: const HomePage(),
         bottomNavigationBar: BottomNavigationBar(
-          selectedItemColor: Colors.blue,
-          unselectedItemColor: Colors.grey,
-          iconSize: 32,
-          elevation: 10,
+          selectedItemColor: Colors.blue, // couleur de l'icône sélectionnée
+          unselectedItemColor: Colors.grey, // couleur des icônes non sélectionnées
+          iconSize: 32, // taille des icônes
+          elevation: 10, // ombre
 
           items: const [
             BottomNavigationBarItem(
@@ -35,5 +49,6 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+
 }
 
